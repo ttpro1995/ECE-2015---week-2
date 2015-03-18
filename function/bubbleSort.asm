cf499936e9a614f520c68cd159c30f1ffed975fc
ThaiThien <tthien@apcs.vn>
2015-03-18 22:23:35 +0700

    done testing bubble sort - appear to correct

--- function/bubbleSort.asm
+++ function/bubbleSort.asm
@@ -3,12 +3,12 @@
 .data
 aa1:	.word	1
 aa2:	.word	3
-aa3:	.word	5
-aa4:	.word	11
+aa3:	.word	2
+aa4:	.word	15
 aa5:	.word	12
-aa6:	.word	15
-aa7:	.word	16
-aa8:	.word	17
+aa6:	.word	25
+aa7:	.word	17
+aa8:	.word	16
 aa9:	.word	18
 aa10:	.word	20
 length:  .word  10
@@ -24,10 +24,13 @@ la $s1, length  	#load address length into s1
 lw $a1, ($s1)		#load arg length
 la $s2, asc		#load address asc into s2
 lw $a2, ($s2)		#load arg asc
-
 jal BubbleSort	#call binary search 
- 
-	
+
+la $a0,aa1 		 #load arg arr
+la $s1, length  	#load address length into s1
+lw $a1, ($s1)		#load arg length
+jal printArray          #print out sorted array
+
 addi $v0,$0,10		#v0 = 10 exit
 syscall			#exit
 
@@ -116,26 +119,6 @@ bne  $t0,$0,forLoop3
 while3:
 bne $s3,$0,whileLoop3  # while (flag) , loop
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 donebubblesort3:
 
 #restore saved register
@@ -151,3 +134,54 @@ addi $sp,$sp,32  #increase stack pointer  by 32 byte
 
 
 jr $ra			#return
+#################################
+printArray:
+# $a0 arr
+# $a1 length
+
+add $s0,$a0,$0  #s0 = base address of arr
+add $s1,$a1,$0  #s1 = length
+addi $s6,$0,8   #space bar
+addi $s2,$0,0  # $s2 = i = 0
+j forPrint
+forPrintLoop:
+#for (int i=0;i<length;i++)
+ # print(a[i])
+add $t5,$s2,$s2  # t5 =i*2
+add $t5,$t5,$t5  # t5 = 4*t5
+add $t5,$t5,$s0   #t5 = address of arr[i]
+lw  $t6,($t5)    #t6 = arr[i]
+
+add $a0,$t6,$0 #load arr[i] into $a0 to print
+addi $v0,$0,1  # $v0 = 1 print int
+
+addi $sp,$sp,-4 #adjust stack pointer
+sw $ra,($sp)     #store $ra
+syscall   #print arr[i]
+
+jal new_line  # go to new line
+
+lw $ra,($sp)   #restore %ra
+addi $sp,$sp,4    #adjust stack pointer
+
+ 
+addi $s2,$s2,1   #increase i   
+forPrint:
+slt $t0,$s2,$s1  # i<length then $t0 =1
+bne $t0, $0, forPrintLoop #continue loop
+
+jr $ra
+
+#########################
+############new_line############
+new_line: # go to next line
+addi $sp,$sp,-4 # decrease $sp by 4
+addi $t0,$0,10  # $t0 = 10 = LF ascii code
+sw $t0,0($sp) #store LF into stack
+addi $a0,$sp,0  #$a0 = address of LF 
+addi $v0,$0,4	# $v0=4, print_string
+syscall		#print_string 
+addi $sp,$sp,4 # increase $sp by 4
+jr $ra
+
+#########################
